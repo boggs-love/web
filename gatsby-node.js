80b5b49da4213e9bba300051d59e00b8dccb6e60
David Barratt <david@davidwbarratt.com>
2018-03-11 16:04:09 -0400

    Add docker

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-exports.modifyWebpackConfig = ({ config, _stage }) => {
+exports.modifyWebpackConfig = ({ config, stage }) => {
   // Make Modernizr a global since it only runs on the browser.
   config.loader('expose-loader?Modernizr', {
     test: /\.modernizrrc(\.json)?$/,
@@ -10,14 +10,39 @@ exports.modifyWebpackConfig = ({ config, _stage }) => {
     test: /\.modernizrrc(\.json)?$/,
   });
 
-  config.merge({
-    resolve: {
-      alias: {
-        styles: path.resolve(config._config.context, 'styles'),
-        app: path.resolve(config._config.context, 'src'),
-        modernizr$: path.resolve(config._config.context, '.modernizrrc'),
+  config.merge(current => (
+    {
+      ...current,
+      resolve: {
+        ...current.resolve,
+        alias: {
+          ...current.resolve.alias,
+          styles: path.resolve(current.context, 'styles'),
+          app: path.resolve(current.context, 'src'),
+          modernizr$: path.resolve(current.context, '.modernizrrc'),
+        },
       },
-    },
+    }
+  ));
+
+  config.merge((current) => {
+    switch (stage) {
+      case 'develop':
+        // Replace the paths with the reverse-proxy.
+        return {
+          ...current,
+          output: {
+            ...current.output,
+            publicPath: current.output.publicPath.replace('0.0.0.0:80', '127.0.0.1:8888'),
+          },
+          entry: {
+            ...current.entry,
+            commons: current.entry.commons.map(entry => entry.replace('0.0.0.0:80', '127.0.0.1:8888')),
+          },
+        };
+      default:
+        return current;
+    }
   });
 
   return config;
