d25abcc1916a05a3c259b6a65e4ad3d3df9f0278
David Barratt <david@davidwbarratt.com>
2018-05-13 09:37:34 -0400

    Update color and add hotel

--- src/components/sections/travel.js
+++ src/components/sections/travel.js
@@ -7,16 +7,19 @@ const Travel = ({ travel }) => {
     return null;
   }
 
+  const locations = travel.edges.map(({ node }) => {
+    const Link = node.data.url ? 'a' : 'span';
 
-  const locations = travel.edges.map(({ node }) => (
-    <div key={node.data.id} className="mb-5 mt-5 text-center">
-      <h6>{node.data.name}</h6>
-      <div>
-        <a href={node.data.url} className="d-block">{node.data.title}</a>
-        {node.data.address.map(line => (<div key={line}>{line}</div>))}
+    return (
+      <div key={node.data.id} className="mb-5 mt-5 text-center">
+        <h6>{node.data.name}</h6>
+        <div>
+          <Link href={node.data.url} className="d-block">{node.data.title}</Link>
+          {node.data.address.map(line => (<div key={line}>{line}</div>))}
+        </div>
       </div>
-    </div>
-  ));
+    );
+  });
 
   return (
     <Section title="Travel">
