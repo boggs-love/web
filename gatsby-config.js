b5c6bb403e3db981f36f25873f0b27ea7b4ae9d3
David Barratt <david@davidwbarratt.com>
2018-06-30 12:32:28 -0400

    Add Sentry

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,12 @@ module.exports = {
     'gatsby-plugin-sharp',
     'gatsby-plugin-react-helmet',
     {
+      resolve: 'gatsby-plugin-sentry',
+      options: {
+        dsn: 'https://0358d4baa01f428e9b5b0c1d54b1df44@sentry.io/1235411',
+      },
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'content',
