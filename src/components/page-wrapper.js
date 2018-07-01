ae7f5de74d7506aa603af5623d202ac98c178d7a
David Barratt <david@davidwbarratt.com>
2018-07-01 10:18:06 -0400

    Add the wedding details and fix the spacing

--- src/components/page-wrapper.js
+++ src/components/page-wrapper.js
@@ -8,6 +8,17 @@ const PageWrapper = ({ background, position, children }) => (
     <div className="container">
       <div className="row justify-content-center">
         <div className="col-12 col-md-10 col-lg-8 col-xl-7 pl-0 pr-0">
+          <div className="row justify-content-center m-0">
+            <div className="col-11 col-sm-12 p-0">
+              <div className="details row align-content-end align-items-end justify-content-between flex-wrap m-0">
+                <h5 className="text-center w-100 w-md-auto">
+                  <span className="text-nowrap">232 N Jungle Rd.</span> <span className="text-nowrap">Geneva, Florida</span>
+                </h5>
+                <h5 className="text-nowrap">10.20.18</h5>
+                <h5 className="text-nowrap">4:30 PM</h5>
+              </div>
+            </div>
+          </div>
           <section className="pt-4">
             <div className="container">
               {children}
